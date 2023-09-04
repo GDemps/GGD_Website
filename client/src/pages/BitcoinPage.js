5e099b761c09a6f4a3962da685c8361cad101ec8
Grant Dempsey <CryptoDemps@gmail.com>
2023-09-04 00:31:08 -0600

    Add What is Bitcoin section, video, and resources

--- client/src/pages/BitcoinPage.js
+++ client/src/pages/BitcoinPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import imageSource from '../Bitcoin_image.jpg'
-import videoSource from '../CongressBuyBitcoin.mp4'; 
+import videoSource from '../BitcoinExplanation.mp4'; 
 import VideoModal from '../components/VideoModal';
 import anotherImage from '../BTC_mining.jpg';
 import bitcoinLogo from '../BTClogo.png';
@@ -29,17 +29,64 @@ const BitcoinPage = () => {
   return (
     <div>
       <div style={{ textAlign: 'center', backgroundColor: 'gray', height: '200px' }}>
-        <img src={anotherImage} alt="Banner" style={{ width: '100%', height: '800px', objectFit: 'cover' }} />
+        <img src={anotherImage} alt="Banner" style={{ width: '100%', height: '800px', objectFit: 'cover' }} />     
       </div>
       <br />
       <div style={{ marginTop: '550px', padding: '20px' }}>
-        <h1>Bitcoin Page</h1>
         <VideoModal isOpen={isModalOpen} onClose={closeModal} />
-        {/* <img src={imageSource} alt="./Bitcoin_image.jpg" /> */}
         <br />
-        <video src={videoSource} autoPlay loop />
-        <br />
-        <br />
+        
+        <div style={{ textAlign: 'center', marginTop: '20px', padding: '20px' }}>
+        <h1>Bitcoin Page</h1>
+        <h2>What is Bitcoin?</h2>
+        <video
+          src={videoSource}
+          autoPlay
+          controls // Add controls to allow users to play/pause and adjust volume
+          style={{ width: '50%', height: '50%' }}
+        />
+        </div>
+        <div style={{ marginTop: '20px', padding: '20px' }}>
+          <h2>Bitcoin Resources</h2>
+          <h3>Documentaries</h3>
+          <ul style={{ listStyleType: 'none', paddingLeft: '0' }}>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <span style={{ marginRight: '10px', backgroundImage: `url(${bitcoinLogo})`, backgroundSize: 'contain', width: '24px', height: '24px', display: 'inline-block' }}></span>
+              <a href="https://www.amazon.com/gp/video/detail/B01MTQZOCV/ref=atv_dp_share_cu_r">Banking on Bitcoin</a>
+            </li>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <span style={{ marginRight: '10px', backgroundImage: `url(${bitcoinLogo})`, backgroundSize: 'contain', width: '24px', height: '24px', display: 'inline-block' }}></span>
+              <a href="https://www.amazon.com/gp/video/detail/B088Q5BYVR/ref=atv_dp_share_cu_r">Banking on Africa: The Bitcoin Revolution</a>
+            </li>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <span style={{ marginRight: '10px', backgroundImage: `url(${bitcoinLogo})`, backgroundSize: 'contain', width: '24px', height: '24px', display: 'inline-block' }}></span>
+              <a href="https://www.amazon.com/gp/video/detail/B00OGM2ZY2/ref=atv_dp_share_cu_r">The Rise and Rise of Bitcoin</a>
+            </li>
+            <h3>Books</h3>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <span style={{ marginRight: '10px', backgroundImage: `url(${bitcoinLogo})`, backgroundSize: 'contain', width: '24px', height: '24px', display: 'inline-block' }}></span>
+              <a href="https://www.audible.com/pd/The-Bitcoin-Standard-Audiobook/B07D7ZRKLJ?action_code=ASSGB149080119000H&share_location=pdp">The Bitcoin Standard</a>
+            </li>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <span style={{ marginRight: '10px', backgroundImage: `url(${bitcoinLogo})`, backgroundSize: 'contain', width: '24px', height: '24px', display: 'inline-block' }}></span>
+              <a href="https://www.audible.com/pd/The-Price-of-Tomorrow-Audiobook/B08724Y81K?action_code=ASSGB149080119000H&share_location=pdp">The Price of Tomorrow</a>
+            </li>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <span style={{ marginRight: '10px', backgroundImage: `url(${bitcoinLogo})`, backgroundSize: 'contain', width: '24px', height: '24px', display: 'inline-block' }}></span>
+              <a href="https://www.audible.com/pd/Bitcoin-Audiobook/B0B77W3XSW?action_code=ASSGB149080119000H&share_location=pdp">Bitcoin: Everything Divided by 21 Million</a>
+            </li>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <span style={{ marginRight: '10px', backgroundImage: `url(${bitcoinLogo})`, backgroundSize: 'contain', width: '24px', height: '24px', display: 'inline-block' }}></span>
+              <a href="https://www.audible.com/pd/The-Bullish-Case-for-Bitcoin-Audiobook/B09QXR9GVL?action_code=ASSGB149080119000H&share_location=pdp">The Bullish Case for Bitcoin</a>
+            </li>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <span style={{ marginRight: '10px', backgroundImage: `url(${bitcoinLogo})`, backgroundSize: 'contain', width: '24px', height: '24px', display: 'inline-block' }}></span>
+              <a href="https://www.audible.https://www.audible.com/pd/Check-Your-Financial-Privilege-Audiobook/B0B9T6BJC4?action_code=ASSGB149080119000H&share_location=pdp/pd/The-Bullish-Case-for-Bitcoin-Audiobook/B09QXR9GVL?action_code=ASSGB149080119000H&share_location=pdp">Check Your Financial Priviledge</a>
+            </li>
+            <h3>Podcasts</h3>
+            
+          </ul>
+        </div>
         <iframe src="https://www.timechaincalendar.com/en" style={{ width: '100%', height: '750px' }}></iframe>
         <div style={{ display: 'flex', alignItems: 'center' }}>
           <p style={{ margin: 0, marginRight: '8px' }}>
@@ -48,22 +95,6 @@ const BitcoinPage = () => {
           <a href="https://www.timechaincalendar.com/en" target="_blank" rel="noopener noreferrer">
             www.timechaincalendar.com/en
           </a>
-        </div>
-        <br />
-        <Link to="/">Go Back to Home</Link>
-        <div style={{ marginTop: '20px' }}>
-          <h2>Bitcoin Resources</h2>
-          <ul style={{ listStyleType: 'none', paddingLeft: '0' }}>
-            <li style={{ display: 'flex', alignItems: 'center' }}>
-              <span style={{ marginRight: '10px', backgroundImage: `url(${bitcoinLogo})`, backgroundSize: 'contain', width: '24px', height: '24px', display: 'inline-block' }}></span>
-              <a href="https://www.example.com/documentary1">Documentary 1</a>
-            </li>
-            <li style={{ display: 'flex', alignItems: 'center' }}>
-              <span style={{ marginRight: '10px', backgroundImage: `url(${bitcoinLogo})`, backgroundSize: 'contain', width: '24px', height: '24px', display: 'inline-block' }}></span>
-              <a href="https://www.example.com/documentary2">Documentary 2</a>
-            </li>
-            {/* Add more links with Bitcoin logo bullets */}
-          </ul>
         </div>
       </div>
     </div>
